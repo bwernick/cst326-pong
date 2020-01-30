884af68e9b11326156c5b2d9c1dc150143203c1e
bwernick <bwernick@csumb.edu>
2020-01-29 16:57:39 -0800

    Pong basics updates

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -5,22 +5,26 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
 
+	private Rigidbody rb;      //Reference to Rigidbody Component
     public float speed = 5f;
+	public float acceleration = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
+		rb = GetComponent<Rigidbody> ();
+		
         //Random starting direction
         float randX = Random.Range(0, 2) == 0 ? -1 : 1;
         float randY = Random.Range(0, 2) == 0 ? -1 : 1;
 
         //Set the velocity of the ball, which has a rigidbody
-        GetComponent<Rigidbody>().velocity = new Vector3(speed * randX, speed * randY, 0f);
+        rb.velocity = new Vector3(speed * randX, speed * randY, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+		
     }
 }
---
+++ Assets/Paddle.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    public float speed = 5f;
+
+	public bool isPaddleA;
+	
+    public Transform paddle_Top;
+    public Transform paddle_Bottom;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+
+        if(isPaddleA){
+			if (Input.GetKey(KeyCode.W))
+			{
+				transform.position = Vector3.MoveTowards(transform.position, paddle_Top.position, step);
+			}
+			if (Input.GetKey(KeyCode.S))
+			{
+				transform.position = Vector3.MoveTowards(transform.position, paddle_Bottom.position, step);
+			}
+		}else{
+			if (Input.GetKey(KeyCode.I))
+			{
+				transform.position = Vector3.MoveTowards(transform.position, paddle_Top.position, step);
+			}
+			if (Input.GetKey(KeyCode.K))
+			{
+				transform.position = Vector3.MoveTowards(transform.position, paddle_Bottom.position, step);
+			}
+		}
+    }
+}
