f1ef31d4bed01d18a6715294786bcae6bbcd7fb8
bwernick <bwernick@csumb.edu>
2020-02-09 20:13:02 -0800

    powerups

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,18 +5,26 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
 
-	private Rigidbody rb;      //Reference to Rigidbody Component
+    private Rigidbody rb;      //Reference to Rigidbody Component
     //private float base_speed = 5f;
     public float speed = 5f;
-	public float acceleration = 5f;
+    public float acceleration = 5f;
+
+    private int lastPaddle;
+
+    public GameObject paddleA;
+    public GameObject paddleB;
+
+    public GameObject powerUp1;
+    public GameObject PowerUp2;
 
     public AudioClip clip;
 
     // Start is called before the first frame update
     void Start()
     {
-		rb = GetComponent<Rigidbody> ();
-		
+        rb = GetComponent<Rigidbody>();
+
         //Random starting direction
         float randX = Random.Range(0, 2) == 0 ? -1 : 1;
         float randY = Random.Range(0, 2) == 0 ? -1 : 1;
@@ -32,6 +40,7 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Accelerate ball over time
         Vector3 normalizedVel = rb.velocity.normalized;
         rb.AddForce(normalizedVel * acceleration);
 
@@ -40,6 +49,8 @@ public class Ball : MonoBehaviour
 
     void OnCollisionEnter(Collision col)
     {
+
+        //movement & startup based on what goal was scored on
         if (col.gameObject.name == "goalA")
         {
             transform.position = new Vector3(0, 0, 1);
@@ -56,9 +67,36 @@ public class Ball : MonoBehaviour
             rb.velocity = new Vector3(speed * temp_x, speed * randY, 0f);
         }
 
-        if((col.gameObject.name == "paddleA") || (col.gameObject.name == "paddleB"))
+        //last paddle to hit ball
+        if (col.gameObject.name == "paddleA")
+        {
+            lastPaddle = 1;
+        }
+
+        if (col.gameObject.name == "paddleB")
+        {
+            lastPaddle = 2;
+        }
+
+        //paddle hit sound
+        if ((col.gameObject.name == "paddleA") || (col.gameObject.name == "paddleB"))
         {
             GetComponent<AudioSource>().Play();
+        }
+
+        if (col.gameObject.name == "powerUp1")
+        {
+            Debug.Log("hit power up 1");
+            Destroy(col.gameObject);
+            StartCoroutine(Grav(rb));
+        }
+
+        if (col.gameObject.name == "powerUp2")
+        {
+            Debug.Log("hit power up 2");
+            Destroy(col.gameObject);
+            Vector3 normalizedVel = rb.velocity.normalized;
+            rb.AddForce(normalizedVel * acceleration);
         }
     }
 
@@ -72,5 +110,19 @@ public class Ball : MonoBehaviour
         rb.velocity = Vector3.zero;
         transform.position = new Vector3(0, 0, 1);
         //Start();
+    }
+
+    //Runs concurrently
+    IEnumerator Grav(Rigidbody rigidbody)
+    {
+        float timer = 4f; //set the time for which to hold gravity off
+
+        while (timer >= 0)
+        {
+            rigidbody.AddForce(0, -5f, 0);
+            timer -= Time.deltaTime; //minus the time that the frame took
+            yield return null; //move to the next frame
+        }
+
     }
 }
--- Assets/Scripts/ScoreScript.cs
+++ Assets/Scripts/ScoreScript.cs
@@ -6,6 +6,11 @@ using UnityEngine.UI;
 public class ScoreScript : MonoBehaviour
 {
 
+    //color text numbers dict for calling on score textbox.
+    string[] scoreText = new string[13]{"<color=white>0</color>", "<color=orange>1</color>", "<color=red>2</color>", "<color=purple>3</color>", 
+                                        "<color=aqua>4</color>", "<color=lightblue>5</color>", "<color=fuchsia>6</color>", "<color=lime>7</color>",
+                                        "<color=olive>8</color>", "<color=#03fc77ff>9</color>", "<color=#fc0398ff>10</color>", "<color=#b8fcfbff>11</color>", 
+                                        "<color=white>12</color>"};
     public Text textbox;
     public GameManagerScript manager;
 
@@ -20,6 +25,8 @@ public class ScoreScript : MonoBehaviour
     {
         int A = manager.playerAScore;
         int B = manager.playerBScore;
-        textbox.text = A + " : " + B;
+
+        //C# can handle markup in text
+        textbox.text = scoreText[A] + " : " + scoreText[B];
     }
 }
